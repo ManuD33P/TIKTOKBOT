443f19b21063cd8184802e2556c509a27e7ece92
Manu16 <116045677+ManuD33P@users.noreply.github.com>
2025-06-10 05:51:18 -0300

    complete options settings

--- src/components/checkbox.jsx
+++ src/components/checkbox.jsx
@@ -3,8 +3,6 @@ import { useEffect } from "react";
 
 
 export default function CheckboxFunction(state,onChangePreferents,key){
-    console.log('esto es state', state)
-    console.log('esto es key',key)
     return (
         state ? 
         <Checkbox 
